45383baa2dc63556930d9413ff8a3eaa94f09997
Austin Wells <acwells@smu.edu>
2014-12-27 14:57:29 -0600

    adding insert script

---
+++ Hack_ID/database/insert.sql
@@ -0,0 +1,9 @@
+USE `hackid` ;
+
+INSERT INTO User(email, name, phone, allergies, school, age, rfid, notes, gender) VALUES
+	("a@smu.edu","a w","123-234-345","none","SMU",13,"1", "I like cookies", "male"),
+	("b@smu.edu","b w","123-234-346","none","SMU",14,"1234", "I live cookies", "female"),
+	("c@smu.edu","c w","123-234-347","none","TCU",143,"1234abcd", "I kinda like cookies", "male"),
+	("d@smu.edu","d w","123-234-348","none","UTD",15,"abcd1234", "I like cookies", "female"),
+	("e@smu.edu","e w","123-234-349","none","ETU",16,"2", "I don't like cookies", "male"),
+	("f@smu.edu","f w","123-234-310","none","RAR",17,"3", "I hate cookies", "female");
